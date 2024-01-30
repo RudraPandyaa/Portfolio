39a2ade84ba95b0cc16094fb7052ebd80ad9def0
Faizanahmed Saiyed <119733178+Faizanahmedsy@users.noreply.github.com>
2024-01-30 06:55:52 +0530

    style: changed design of experience

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import BentoGrid from "@/components/modules/BentoGrid/BentoGrid";
 import NameCard from "@/components/modules/NameCard/NameCard";
 import TechStack from "@/components/modules/TechStack/TechStack";
 import { UiWrapper } from "@/components/shared";
+import { Card, CardDescription, CardTitle } from "@/components/ui/card";
 
 export default function Home() {
   return (
@@ -9,6 +10,33 @@ export default function Home() {
       <NameCard />
 
       <TechStack />
+
+      {/* <UiWrapper>
+        <div className="grid grid-cols-2 gap-4 ">
+          <Card className="px-2 py-5">
+            <CardTitle
+              className="px-2 py-2
+                "
+            >
+              Devstree It Services
+            </CardTitle>
+            <CardDescription className="px-2 py-2">
+              Jr Software Developer | April 2023 - Present <br />
+            </CardDescription>
+          </Card>
+          <Card className="px-2 py-5">
+            <CardTitle
+              className="px-2 py-2
+                "
+            >
+              Viji Foods
+            </CardTitle>
+            <CardDescription className="px-2 py-2">
+              Customer Service Team Lead | Jan 2022 - Nov 2022 <br />
+            </CardDescription>
+          </Card>
+        </div>
+      </UiWrapper> */}
 
       <BentoGrid />
     </>
--- src/components/modules/BentoGrid/BentoGrid.tsx
+++ src/components/modules/BentoGrid/BentoGrid.tsx
@@ -7,25 +7,56 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 
+import dayjs from "dayjs";
+
 const BentoGrid = () => {
+  // const currentDate = new Date();
+
+  // const startDate = new Date("April 2023");
+
+  // const monthsDiff =
+  //   (currentDate.getFullYear() - startDate.getFullYear()) * 12 +
+  //   currentDate.getMonth() -
+  //   startDate.getMonth();
+
+  // const years = Math.floor(monthsDiff / 12);
+  // const remainingMonths = monthsDiff % 12;
+
+  //TODO: FIX THIS CALCULATION
+
+  const currentDate = dayjs();
+
+  const startDate = dayjs("2023-04-01");
+
+  const monthsDiff = currentDate.diff(startDate, "month");
+
+  const years = Math.floor(monthsDiff / 12);
+  const remainingMonths = monthsDiff % 12;
+
+  const experienceText = `Total of ${years} years and ${remainingMonths} months of experience in Corporate`;
+
+  console.log("years", years);
+  console.log("remainingMonths", remainingMonths);
+
   return (
     <UiWrapper>
       <div className="grid grid-cols-3 gap-4">
-        <div className="col-span-2">
+        <div className="col-span-3">
+          {/* //TODO: Make this dynamic as per the months spent in devstree */}
           <Card className="w-full h-full">
-            <CardHeader>
+            <CardHeader className="text-center">
               <CardTitle>Experience</CardTitle>
               <CardDescription>
-                Total of 2 years of experience in Corporate
+                Total of 1 year and 9 Months of experience in Corporate
               </CardDescription>
             </CardHeader>
-            <div className="grid grid-cols-2 gap-4 p-6">
+            <div className="grid grid-cols-2 gap-4 px-6 pb-4">
               <Card className="px-2 py-5">
                 <CardTitle
                   className="px-2 py-2
                 "
                 >
-                  Devstree It
+                  Devstree It Services
                 </CardTitle>
                 <CardDescription className="px-2 py-2">
                   Jr Software Developer | April 2023 - Present <br />
