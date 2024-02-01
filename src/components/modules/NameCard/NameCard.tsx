bf949744e50ff580f19ed156bc8c4bd41ff9dfd6
Faizanahmed Saiyed <119733178+Faizanahmedsy@users.noreply.github.com>
2024-02-01 10:29:55 +0530

    feat: added a new background gradiant

--- src/components/modules/NameCard/NameCard.tsx
+++ src/components/modules/NameCard/NameCard.tsx
@@ -1,19 +1,25 @@
 import { UiWrapper } from "@/components/shared";
 import { Card } from "@/components/ui/card";
 import AnimatedName from "./_components/AnimatedName";
+import styles from "./NameCard.module.css";
 
 const NameCard = () => {
   return (
     <UiWrapper>
-      <Card className="flex flex-col justify-center items-center h-[620px]">
-        <h1 className="flex font-bold text-center text-6xl animate-fade-in [--ani-delay:200ms] opacity-0 translate-y-[-1rem]">
-          <AnimatedName />
-        </h1>
-        <h2 className="flex font-bold text-center text-3xl animate-fade-in [--ani-delay:2000ms] opacity-0 translate-y-[-1rem] text-slate-500">
-          a Frontend Developer
-        </h2>
-        <div className="flex"></div>
-      </Card>
+      <div className="overflow-hidden">
+        {" "}
+        <Card className="flex flex-col justify-center items-center h-[620px]">
+          <h1 className="flex font-bold text-center text-6xl animate-fade-in [--ani-delay:200ms] opacity-0 translate-y-[-1rem]">
+            <AnimatedName />
+          </h1>
+          <h2 className="flex font-bold text-center text-3xl animate-fade-in [--ani-delay:2000ms] opacity-0 translate-y-[-1rem] text-slate-500">
+            a Frontend Developer
+          </h2>
+          <div className="flex"></div>
+          <div className={styles.topBlur} />
+          <div className={styles.bottomBlur} />
+        </Card>
+      </div>
     </UiWrapper>
   );
 };
