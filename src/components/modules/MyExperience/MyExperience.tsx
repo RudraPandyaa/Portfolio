0644e72c67ed48985f13526f643d72aa2474be04
Faizanahmedsy <saiyed.faizanahmed1@gmail.com>
2024-06-16 12:26:00 +0530

    enacnced exp and edu sec

--- src/components/modules/MyExperience/MyExperience.tsx
+++ src/components/modules/MyExperience/MyExperience.tsx
@@ -11,18 +11,32 @@ export default function MyExperience() {
         <SectionTitle>Experience & Education</SectionTitle>
         <div className="grid md:grid-cols-2 gap-4">
           <SimpleCard className="col-span-2">
-            <div className="flex ">
-              <div className="bg-violet-200 text-violet-800 rounded-l-3xl w-full p-6 text-end">
-                <Subtitle>{experienceData[0].subRoles[0].role}</Subtitle>
-                <p className="text-sm">
-                  {experienceData[0].company} | April 2023 - Present <br />
-                </p>
+            <div className="flex bg-slate-50 rounded-3xl text-black">
+              <div className="text-end">
+                <div className="bg-violet-200 text-violet-800 rounded-l-3xl  w-full p-6 ">
+                  <Subtitle>{experienceData[0].subRoles[0].role}</Subtitle>
+                  <p className="text-sm">
+                    {experienceData[0].company} | April 2023 - Present <br />
+                  </p>
+                </div>
+                <div className="p-4">
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Culpa autem reiciendis repudiandae consectetur maxime
+                  excepturi ad, consequatur hic! Voluptatibus, minima!
+                </div>
               </div>
-              <div className="bg-[#9ecac9] text-[#0d4644] rounded-r-3xl  w-full p-6">
-                <Subtitle>{collageData.degreeShort}</Subtitle>
-                <p className="text-sm">
-                  {collageData.collage} | July 2022 - Present <br />
-                </p>
+              <div>
+                <div className="bg-[#9ecac9] text-[#0d4644] rounded-r-3xl  w-full p-6">
+                  <Subtitle>{collageData.degreeShort}</Subtitle>
+                  <p className="text-sm">
+                    {collageData.collage} | July 2022 - Present <br />
+                  </p>
+                </div>
+                <div className="p-4">
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Culpa autem reiciendis repudiandae consectetur maxime
+                  excepturi ad, consequatur hic! Voluptatibus, minima!
+                </div>
               </div>
             </div>
           </SimpleCard>
