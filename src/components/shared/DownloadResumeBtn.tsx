20d75b6812b25ccce5005bba116f4a6744b5268a
Rudra Pandya <viralpandya079@gmail.com>
2026-01-31 16:16:30 +0530

    Resume updated

--- src/components/shared/DownloadResumeBtn.tsx
+++ src/components/shared/DownloadResumeBtn.tsx
@@ -7,7 +7,7 @@ export default function DownloadResumeBtn({}: Props) {
   return (
     <Link
       href={
-        "https://drive.google.com/file/d/1dKFUxpnZ1_cuLlM2_Tj5ME9MKXqq8VgH/view?usp=sharing/view?usp=sharing"
+        "https://drive.google.com/file/d/1VotjfbCNIxXsql4BuNGd8fe4NYA4z1gT/view?usp=sharing"
       }
       target="_blank"
       className={buttonVariants({
